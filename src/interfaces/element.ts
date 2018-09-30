59e8e5f6ac179c46cf1a35294000bf7445a5004a
James L. Stevenson <jim.stevenson@kogentservices.com>
2018-09-30 17:55:47 -0400

    Added ElementType for paymentRequestButton; added ElementOptions field for paymentRequest.

--- src/interfaces/element.ts
+++ src/interfaces/element.ts
@@ -1,53 +1,61 @@
 export interface Element {
-  mount(el: HTMLElement | string): void;
-  on(ev: ElementEventType, handler: (ev?: any) => void): void;
-  blur(): void;
-  clear(): void;
-  focus(): void;
-  unmount(): void;
-  update(options: ElementOptions): void;
+    mount(el: HTMLElement | string): void;
+
+    on(ev: ElementEventType, handler: (ev?: any) => void): void;
+
+    blur(): void;
+
+    clear(): void;
+
+    focus(): void;
+
+    unmount(): void;
+
+    update(options: ElementOptions): void;
 }
 
 export type ElementEventType = 'blur' | 'change' | 'click' | 'focus' | 'ready';
 
 export type ElementType =
-  | 'card'
-  | 'cardNumber'
-  | 'cardExpiry'
-  | 'cardCvc'
-  | 'postalCode';
+    | 'card'
+    | 'cardNumber'
+    | 'cardExpiry'
+    | 'cardCvc'
+    | 'postalCode'
+    | 'paymentRequestButton';
 
 export interface ElementOptions {
-  style?: {
-    base?: ElementStyleAttributes;
-    complete?: ElementStyleAttributes;
-    empty?: ElementStyleAttributes;
-    invalid?: ElementStyleAttributes;
-  };
-  hidePostalCode?: boolean;
-  hideIcon?: boolean;
-  iconStyle?: 'solid' | 'default';
-  placeholder?: string;
-  value?: string | object;
+    style?: {
+        base?: ElementStyleAttributes;
+        complete?: ElementStyleAttributes;
+        empty?: ElementStyleAttributes;
+        invalid?: ElementStyleAttributes;
+    };
+    hidePostalCode?: boolean;
+    hideIcon?: boolean;
+    iconStyle?: 'solid' | 'default';
+    placeholder?: string;
+    value?: string | object;
+    paymentRequest?: PaymentRequest;
 }
 
 export interface ElementStyleAttributes {
-  color?: any;
-  fontFamily?: any;
-  fontSize?: any;
-  fontSmoothing?: any;
-  fontStyle?: any;
-  fontWeight?: any;
-  fontVariant?: any;
-  iconColor?: any;
-  lineHeight?: any;
-  letterSpacing?: any;
-  textDecoration?: any;
-  textShadow?: any;
-  textTransform?: any;
-  ':hover'?: any;
-  ':focus'?: any;
-  '::placeholder'?: any;
-  '::selection'?: any;
-  ':-webkit-autofill'?: any;
+    color?: any;
+    fontFamily?: any;
+    fontSize?: any;
+    fontSmoothing?: any;
+    fontStyle?: any;
+    fontWeight?: any;
+    fontVariant?: any;
+    iconColor?: any;
+    lineHeight?: any;
+    letterSpacing?: any;
+    textDecoration?: any;
+    textShadow?: any;
+    textTransform?: any;
+    ':hover'?: any;
+    ':focus'?: any;
+    '::placeholder'?: any;
+    '::selection'?: any;
+    ':-webkit-autofill'?: any;
 }
